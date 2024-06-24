42f06db670bd755207b1bb3842b758d54d1f9542
pandora <imcathalat@protonmail.com>
2024-06-24 20:03:57 -0300

    terminei view e controller criarRequisicao

---
+++ codigo/src/main/java/controller/AbrirRequisicaoController.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dao.Requisicoes;
+import java.time.LocalDateTime;
+import javax.swing.JOptionPane;
+import view.AbrirRequisicaoView;
+import model.Requisicao;
+import restaurante.Cliente;
+
+/**
+ *
+ * @author imcat
+ */
+public class AbrirRequisicaoController {
+    private Requisicoes requisicoes;
+    private AbrirRequisicaoView view;
+    
+    
+    
+    public AbrirRequisicaoController(){
+        this.view = new AbrirRequisicaoView();
+        this.requisicoes = Requisicoes.getInstance();
+        
+        this.view.getConfirmaBtn().addActionListener((e) -> {
+            abrirRequisicao();
+        });
+        
+        this.view.getCancelaBtn().addActionListener((e) -> {
+            cancelar();
+        });
+        
+        //this.view.setVisible(true);
+        
+        //adicionar os listeners aqui
+        // 
+    }
+    
+    public void abreView(){this.view.setVisible(true);}
+    
+    public void abrirRequisicao(){
+        //pegar os dados dos campos da view
+        String nomeCliente = view.getNomeClienteField().getText();
+        String cpfCliente = view.getCpfField().getText();
+        int quantPessoas = Integer.parseInt(view.getQuantPessoasField().getText());
+        
+        Cliente cliente = new Cliente(nomeCliente, cpfCliente);
+        Requisicao r = new Requisicao(LocalDateTime.now(), quantPessoas, cliente);
+        
+        requisicoes.addRequisicao(r);
+        
+        JOptionPane.showMessageDialog(view, "Requisicao salva com sucesso!");
+        
+        limparTela();
+        
+        // devemos fazer tratamento de exceção
+        // controller comunica a model com o dao
+    } 
+    
+    private void limparTela(){
+        
+        this.view.getNomeClienteField().setText("");
+        this.view.getCpfField().setText("");
+        this.view.getQuantPessoasField().setText("");
+    }
+    
+    private void cancelar() {
+        this.view.dispose();
+    }
+    
+}
--- codigo/src/main/java/main/Restaurante.java
+++ codigo/src/main/java/main/Restaurante.java
@@ -13,10 +13,12 @@ import restaurante.Cliente;
  *
  * @author imcat
  */
-public class Restaurante {
-    Requisicoes requisicoes = Requisicoes.getInstance();
-    Cliente cliente = new Cliente("Isadora", "078.984.827-78");
-    Requisicao requisicao = new Requisicao(LocalDateTime.now(), 4, cliente);
-    requisicoes.addRequisicao(requisicao);
+public class Restaurante extends javax.swing.JFrame {
     
+    public static void main(String args[]){
+        Requisicoes requisicoesDAO = Requisicoes.getInstance();
+        Cliente cliente = new Cliente("Isadora", "078.984.827-78");
+        Requisicao requisicao = new Requisicao(LocalDateTime.now(), 4, cliente);
+        requisicoesDAO.addRequisicao(requisicao);
+    }
 }
